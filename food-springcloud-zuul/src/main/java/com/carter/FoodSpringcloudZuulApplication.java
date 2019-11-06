1d0cbe15035e1178df3a24bbad5449921c1268ec
carter <1105563004@qq.com>
2019-11-06 17:10:31 +0800

    微服务网关项目

---
+++ food-springcloud-zuul/src/main/java/com/carter/FoodSpringcloudZuulApplication.java
@@ -0,0 +1,13 @@
+package com.carter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodSpringcloudZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodSpringcloudZuulApplication.class, args);
+    }
+
+}
---
+++ food-springcloud-zuul/src/test/java/com/carter/FoodSpringcloudZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodSpringcloudZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
