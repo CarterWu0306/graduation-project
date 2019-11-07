de1124631af2d11c6776646c6596d25f3848d468
carter <1105563004@qq.com>
2019-11-07 14:45:51 +0800

    评价微服务

---
+++ food-springcloud-evaluation/src/test/java/com/carter/FoodSpringcloudEvaluationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carter;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodSpringcloudEvaluationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
