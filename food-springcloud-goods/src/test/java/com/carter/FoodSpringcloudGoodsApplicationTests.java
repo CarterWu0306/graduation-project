887b43383265214c0e8e8ff9b8d30a2da3bd2ab2
carter <1105563004@qq.com>
2019-11-07 14:20:26 +0800

    商品微服务

---
+++ food-springcloud-goods/src/test/java/com/carter/FoodSpringcloudGoodsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carter;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodSpringcloudGoodsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
