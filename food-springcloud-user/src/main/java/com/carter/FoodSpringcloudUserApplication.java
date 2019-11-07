7c90df21358e4b1fc41fa8b5aa883e831b4f7aff
carter <1105563004@qq.com>
2019-11-07 11:06:50 +0800

    用户服务

---
+++ food-springcloud-user/src/main/java/com/carter/FoodSpringcloudUserApplication.java
@@ -0,0 +1,13 @@
+package com.carter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodSpringcloudUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodSpringcloudUserApplication.class, args);
+    }
+
+}
---
+++ food-springcloud-user/src/test/java/com/carter/FoodSpringcloudUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carter;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodSpringcloudUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
