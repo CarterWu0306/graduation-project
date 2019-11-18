05f3499544f735328bd4fd027d7cabdc1da19769
carter <1105563004@qq.com>
2019-11-18 15:28:11 +0800

    修改filter中token验证规则

---
+++ food-springcloud-zuul/src/main/java/com/carter/filter/LoginFilter.java
@@ -0,0 +1,113 @@
+package com.carter.filter;
+
+import com.alibaba.fastjson.JSON;
+import com.carter.common.ResponseBo;
+import com.carter.utils.JWTUtil;
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
+
+/**
+ * 登录过滤器
+ */
+@Component
+public class LoginFilter extends ZuulFilter {
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    /**
+     * 过滤器类型
+     * 前置过滤器
+     * @return
+     */
+    @Override
+    public String filterType() {
+        return PRE_TYPE;
+    }
+
+    /**
+     * 过滤器顺序，越小越先执行
+     */
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    /**
+     * 过滤器是否生效
+     * 返回true代表需要权限校验，false代表不需要用户校验即可访问
+     */
+    @Override
+    public boolean shouldFilter() {
+        //共享RequestContext，上下文对象
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+
+        System.out.println(request.getRequestURI());
+        //不需要权限校验URL
+        if ("/food-user/user/login".equalsIgnoreCase(request.getRequestURI())) {
+            return false;
+        } else if ("/food-user/user/logout".equalsIgnoreCase(request.getRequestURI())) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 业务逻辑
+     * 只有上面返回true的时候，才会进入到该方法
+     */
+    @Override
+    public Object run() throws ZuulException {
+        //JWT
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+
+        //token对象,有可能在请求头传递过来，也有可能是通过参数传过来，实际开发一般都是请求头方式
+        String token = request.getHeader("X-token");
+
+        /*if (StringUtils.isBlank((token))) {
+            token = request.getParameter("X-token");
+        }*/
+        System.out.println("页面传来的token值为：" + token);
+        //登录校验逻辑  如果token为null,或者jwt不合法(过期,被篡改等),则直接返回客户端，而不进行下一步接口调用
+        if (StringUtils.isBlank(token)||!JWTUtil.verify(token)) {
+            // 过滤该请求，不对其进行路由
+            requestContext.setSendZuulResponse(false);
+            //返回错误代码
+            requestContext.setResponseStatusCode(HttpStatus.UNAUTHORIZED.value());
+            //重中之重，这里一定要加要给Response设置CharacterEncoding编码为UTF-8
+            requestContext.getResponse().setCharacterEncoding("UTF-8");
+            requestContext.getResponse().setContentType("application/json;charset=UTF-8");
+            //设置响应体内容
+            String body = JSON.toJSONString(ResponseBo.error(401,"登录信息不存在或已失效,请重新登录"));
+            requestContext.setResponseBody(body);
+        }else{
+            //从Redis校验token是否有效
+            //如果过期
+            if (stringRedisTemplate.getExpire(token)<=0){
+                System.out.println("token过期");
+                // 过滤该请求，不对其进行路由
+                requestContext.setSendZuulResponse(false);
+                //返回错误代码
+                requestContext.setResponseStatusCode(HttpStatus.UNAUTHORIZED.value());
+                //重中之重，这里一定要加要给Response设置CharacterEncoding编码为UTF-8
+                requestContext.getResponse().setCharacterEncoding("UTF-8");
+                requestContext.getResponse().setContentType("application/json;charset=UTF-8");
+                //设置响应体内容
+                String body = JSON.toJSONString(ResponseBo.error(401,"登录信息已失效,请重新登录"));
+                requestContext.setResponseBody(body);
+            }
+        }
+        return null;
+    }
+}
---
+++ food-springcloud-zuul/src/main/java/com/carter/utils/JWTUtil.java
@@ -0,0 +1,59 @@
+package com.carter.utils;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+
+import java.util.Date;
+
+public class JWTUtil {
+    public static final String SECRET_KEY = "123456"; //秘钥
+    public static final int TOKEN_EXPIRE_TIME = 30 * 60 * 1000; //token过期时间(毫秒)
+    public static final int REFRESH_TOKEN_EXPIRE_TIME = 60 * 60 * 1000; //refreshToken过期时间(毫秒)
+    private static final String ISSUER = "issuer"; //签发人
+
+    /**
+     * 生成签名
+     */
+    public static String generateToken(String username){
+        Date now = new Date();
+        Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY); //算法
+
+        String token = JWT.create()
+                .withIssuer(ISSUER) //签发人
+                .withIssuedAt(now) //签发时间
+                .withExpiresAt(new Date(now.getTime() + TOKEN_EXPIRE_TIME)) //过期时间
+                .withClaim("username", username) //保存身份标识
+                .sign(algorithm);
+        return token;
+    }
+
+    /**
+     * 验证token,合法性,是否过期,是否被篡改
+     */
+    public static boolean verify(String token){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(SECRET_KEY); //算法
+            JWTVerifier verifier = JWT.require(algorithm)
+                    .withIssuer(ISSUER)
+                    .build();
+            verifier.verify(token);
+            return true;
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return false;
+    }
+
+    /**
+     * 从token获取username
+     */
+    public static String getUsername(String token){
+        try{
+            return JWT.decode(token).getClaim("username").asString();
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
+        return "";
+    }
+}
