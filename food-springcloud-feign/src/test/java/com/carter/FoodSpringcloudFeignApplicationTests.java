45cbb0445f58a9eb35cd3b375f571cd48cf63942
carter <1105563004@qq.com>
2019-11-21 17:06:41 +0800

    服务继承feign

---
+++ food-springcloud-feign/src/test/java/com/carter/FoodSpringcloudFeignApplicationTests.java
@@ -0,0 +1,14 @@
+package com.carter;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodSpringcloudFeignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
