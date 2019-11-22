43b759e11179a14f414594fda94f84d3b6af6fe1
carter <1105563004@qq.com>
2019-11-22 10:51:56 +0800

    启动类添加注解

--- food-springcloud-order/src/main/java/com/carter/FoodSpringcloudOrderApplication.java
+++ food-springcloud-order/src/main/java/com/carter/FoodSpringcloudOrderApplication.java
@@ -1,9 +1,15 @@
 package com.carter;
 
+import com.codingapi.txlcn.tc.config.EnableDistributedTransaction;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
+@EnableDistributedTransaction
 @SpringBootApplication
+@MapperScan("com.carter.mapper")
 public class FoodSpringcloudOrderApplication {
 
     public static void main(String[] args) {
