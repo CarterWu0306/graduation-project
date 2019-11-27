4b72cefef958340923a559fb7e52b0366122527a
carter <1105563004@qq.com>
2019-11-27 10:52:12 +0800

    修改接口

--- food-springcloud-feign/src/main/java/com/carter/controller/PlaceOrderController.java
+++ food-springcloud-feign/src/main/java/com/carter/controller/PlaceOrderController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.math.BigDecimal;
 
 @RestController
-@RequestMapping("feign")
+@RequestMapping("placeOrder")
 public class PlaceOrderController {
 
     @Autowired
