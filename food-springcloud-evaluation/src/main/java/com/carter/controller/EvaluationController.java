e1187d13caf508bb3a56d23a35d2a1b5fc644b19
carter <1105563004@qq.com>
2019-12-04 13:55:41 +0800

    修改接口地址

--- food-springcloud-evaluation/src/main/java/com/carter/controller/EvaluationController.java
+++ food-springcloud-evaluation/src/main/java/com/carter/controller/EvaluationController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/evaluation")
 public class EvaluationController {
 
     @Autowired
