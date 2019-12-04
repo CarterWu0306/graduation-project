55f8ba4c43b34f77662b11173b6f883443903be8
carter <1105563004@qq.com>
2019-12-04 14:58:23 +0800

    修改接口地址

--- food-springcloud-feign/src/main/java/com/carter/controller/AppriseOrderController.java
+++ food-springcloud-feign/src/main/java/com/carter/controller/AppriseOrderController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("appriseOrder")
 public class AppriseOrderController {
 
     @Autowired
